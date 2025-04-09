4493939a0837dbf814fc4b34dbd8759c5eb27244
dd0n9 <dongbass14@gmail.com>
2025-04-09 19:45:22 +0800

    Registry service to LocalRegistry

--- example-provider/src/main/java/com/dd/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/dd/example/provider/EasyProviderExample.java
@@ -1,13 +1,18 @@
 package com.dd.example.provider;
 
+import com.dd.drpc.registry.LocalRegistry;
 import com.dd.drpc.server.HttpServer;
 import com.dd.drpc.server.VertxHttpServer;
+import com.dd.example.common.service.UserService;
 
 /**
  * 简易提供者启动类
  */
 public class EasyProviderExample {
     public static void main(String[] args) {
+        // 注册服务到本地注册器
+        LocalRegistry localRegistry = new LocalRegistry();
+        localRegistry.register(UserService.class.getName(), UserServiceImpl.class);
         // 启动web服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
