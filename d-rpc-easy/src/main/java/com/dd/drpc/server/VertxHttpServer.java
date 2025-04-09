ab16f8eb4a6590e617eb09ade4fff5c2433aa489
dd0n9 <dongbass14@gmail.com>
2025-04-09 19:18:39 +0800

    build a web server

---
+++ d-rpc-easy/src/main/java/com/dd/drpc/server/VertxHttpServer.java
@@ -0,0 +1,33 @@
+package com.dd.drpc.server;
+
+import io.vertx.core.Vertx;
+
+public class VertxHttpServer implements HttpServer{
+
+
+    @Override
+    public void doStart(int port) {
+        // 创建vertx实例
+        Vertx vertx = Vertx.vertx();
+        // 创建http服务器
+        io.vertx.core.http.HttpServer httpServer = vertx.createHttpServer();
+
+        // 处理请求
+        httpServer.requestHandler(request -> {
+            System.out.println("Received request:" + request.method() + " " + request.uri());
+
+            request
+                    .response()
+                    .putHeader("content-type", "text/plain")
+                    .end("Hello World");
+        });
+
+        httpServer.listen(port, result -> {
+            if (result.succeeded()) {
+                System.out.println("Server started on port: " + port);
+            } else {
+                System.out.println("Failed to start server on port: " + result.cause());
+            }
+        });
+    }
+}
--- example-provider/src/main/java/com/dd/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/dd/example/provider/EasyProviderExample.java
@@ -1,11 +1,15 @@
 package com.dd.example.provider;
 
+import com.dd.drpc.server.HttpServer;
+import com.dd.drpc.server.VertxHttpServer;
+
 /**
  * 简易提供者启动类
  */
 public class EasyProviderExample {
     public static void main(String[] args) {
-        // TODO 提供服务
-
+        // 启动web服务
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080);
     }
 }
