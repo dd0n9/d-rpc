7b212a1a9739175c9a48ce448bf63d25a0f77d22
dd0n9 <dongbass14@gmail.com>
2025-04-12 15:15:07 +0800

    build a KyroSerializer

---
+++ d-rpc-core/src/main/java/com/dd/drpc/serializer/KyroSerializer.java
@@ -0,0 +1,36 @@
+package com.dd.drpc.serializer;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+
+public class KyroSerializer implements Serializer {
+    private static final ThreadLocal<Kryo> KRYO_THREAD_LOCAL = ThreadLocal.withInitial(() -> {
+        Kryo kryo = new Kryo();
+        // 设置动态动态序列化和反序列化类，不提前注册所有类（可能有安全问题）
+        kryo.setRegistrationRequired(false);
+        return kryo;
+    });
+
+    @Override
+    public <T> byte[] serialize(T obj) throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Output output = new Output(outputStream);
+        KRYO_THREAD_LOCAL.get().writeObject(output, obj);
+        output.close();
+        return outputStream.toByteArray();
+    }
+
+    @Override
+    public <T> T deserialize(byte[] data, Class<T> clazz) throws Exception {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
+        Input input = new Input(inputStream);
+        T result = KRYO_THREAD_LOCAL.get().readObject(input, clazz);
+        input.close();
+        return result;
+    }
+}
