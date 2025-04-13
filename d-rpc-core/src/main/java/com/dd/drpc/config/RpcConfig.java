96f280ba25cc01e01ab0b600942dae89172d93cb
dd0n9 <dongbass14@gmail.com>
2025-04-13 17:28:49 +0800

    add RegistryConfig

--- d-rpc-core/src/main/java/com/dd/drpc/config/RpcConfig.java
+++ d-rpc-core/src/main/java/com/dd/drpc/config/RpcConfig.java
@@ -38,4 +38,9 @@ public class RpcConfig {
      * 序列化器
      */
     private String serializer = SerializerKeys.JDK;
+
+    /**
+     * 注册中心
+     */
+    private RegistryConfig registryConfig = new RegistryConfig();
 }
