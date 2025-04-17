02f8645a3661fff0b6ac36512a66d4171b6c432f
dd0n9 <dongbass14@gmail.com>
2025-04-17 19:22:18 +0800

    RoundRobinLoadBalancer

---
+++ d-rpc-core/src/main/java/com/dd/drpc/loadbalancer/RoundRobinLoadBalancer.java
@@ -0,0 +1,33 @@
+package com.dd.drpc.loadbalancer;
+
+import com.dd.drpc.model.ServiceMetaInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 轮询负载均衡器
+ */
+public class RoundRobinLoadBalancer implements LoadBalancer {
+
+    /**
+     * 轮询下标
+     */
+    private final AtomicInteger currentIndex = new AtomicInteger(0);
+
+    @Override
+    public ServiceMetaInfo select(Map<String, Object> requestParam, List<ServiceMetaInfo> serviceMetaInfoList) {
+        if (serviceMetaInfoList.isEmpty()) {
+            return null;
+        }
+
+        int size = serviceMetaInfoList.size();
+        if (size == 1) {
+            return serviceMetaInfoList.get(0);
+        }
+
+        int index = currentIndex.getAndIncrement() % size;
+        return serviceMetaInfoList.get(index);
+    }
+}
