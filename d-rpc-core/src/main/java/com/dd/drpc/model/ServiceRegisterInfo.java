75f5268eabdcb012fc933d1c69f207109911a207
dd0n9 <dongbass14@gmail.com>
2025-04-23 14:18:06 +0800

    ProividerBooststrap

--- d-rpc-core/src/main/java/com/dd/drpc/model/ServiceRegisterInfo.java
+++ d-rpc-core/src/main/java/com/dd/drpc/model/ServiceRegisterInfo.java
@@ -1,11 +1,15 @@
 package com.dd.drpc.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 服务注册信息
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServiceRegisterInfo<T> {
 
     /**
--- example-provider/src/main/java/com/dd/example/provider/ProviderExample.java
+++ example-provider/src/main/java/com/dd/example/provider/ProviderExample.java
@@ -1,9 +1,11 @@
 package com.dd.example.provider;
 
 import com.dd.drpc.RpcApplication;
+import com.dd.drpc.bootstrap.ProviderBootstrap;
 import com.dd.drpc.config.RegistryConfig;
 import com.dd.drpc.config.RpcConfig;
 import com.dd.drpc.model.ServiceMetaInfo;
+import com.dd.drpc.model.ServiceRegisterInfo;
 import com.dd.drpc.registry.LocalRegistry;
 import com.dd.drpc.registry.Registry;
 import com.dd.drpc.registry.RegistryFactory;
@@ -11,34 +13,21 @@ import com.dd.drpc.server.HttpServer;
 import com.dd.drpc.server.VertxHttpServer;
 import com.dd.example.common.service.UserService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 提供者启动类
  */
 public class ProviderExample {
     public static void main(String[] args) {
-        // 初始化
-        RpcApplication.init();
 
-        // 注册服务
-        String serviceName = UserService.class.getName();
-        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
-        System.out.println("正在注册服务到注册中心...");
-        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
-        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
-        Registry registry = RegistryFactory.getRegistry(registryConfig.getRegistry());
-        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
-        serviceMetaInfo.setServiceName(serviceName);
-        serviceMetaInfo.setServiceHost(rpcConfig.getHost());
-        serviceMetaInfo.setServicePort(8088);
-        serviceMetaInfo.setServiceVersion(rpcConfig.getVersion());
-        try {
-            registry.register(serviceMetaInfo);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("注册服务到注册中心...");
-        // 启动web服务
-        HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(RpcApplication.getRpcConfig().getPort());
+        // 选择要注册的服务
+        List<ServiceRegisterInfo<?>> serviceRegisterInfoList = new ArrayList<>();
+        ServiceRegisterInfo serviceRegisterInfo = new ServiceRegisterInfo(UserService.class.getName(), UserServiceImpl.class);
+        serviceRegisterInfoList.add(serviceRegisterInfo);
+
+        // 初始化提供者
+        ProviderBootstrap.init(serviceRegisterInfoList);
     }
 }
